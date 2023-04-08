546d86cf0e8e3e192a197991dbda83ff2f35d655
phucvm2023 <130111130+phucvm2023@users.noreply.github.com>
2023-04-08 22:16:33 +0700

    Update main.dart

--- flutter_git001_calculator/lib/main.dart
+++ flutter_git001_calculator/lib/main.dart
@@ -10,9 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: darkBlue,
-      ),
+//      theme: ThemeData.dark().copyWith(
+//        scaffoldBackgroundColor: darkBlue,
+//      ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: Center(
@@ -40,33 +40,246 @@ class _scr006RouteState extends State<scr006Route> {
           title: const Text("Basic Calculator"),
         ),
         body:Center(
-          child: ListView(children: <Widget>[
-            Container(
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                border: Border.all(),
-              ),
-                child: TextButton(
-                  child: const Text(
-                    'Show Details',
-                    style: TextStyle(color: Colors.blueAccent),
+          child: SingleChildScrollView(
+            child: Column(children: <Widget>[
+              Row(
+                children: [
+                  Text(
+                    'Hey!',
+                    style: TextStyle(
+                      fontSize: 50,
+                      fontFamily: 'Futura',
+                      color: Colors.green,
+                    ),
                   ),
-                  onPressed: () => setState(() {
-                    opacity = 1;
-                  }),
-            ),
-//            TextButton(
-//              child: const Text(
-//                'Show Details',
-//                style: TextStyle(color: Colors.blueAccent),
+                ],
+              ),
+
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(4),
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+        //                color: Colors.blue,
+                        border: Border.all(),
+                    ),
+                    child: TextButton(
+                      child: const Text(
+                        'Show Details',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => setState(() {
+                        opacity = 1;
+                      }),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(4),
+                    width: 150,
+                    height: 150,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
 //              ),
-//              onPressed: () => setState(() {
-//                opacity = 1;
-//              }),
-            ),
-          ]),
+                    child: TextButton(
+                      child: const Text(
+                        'Show Details',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => setState(() {
+                        opacity = 1;
+                      }),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(4),
+                    width: 150,
+                    height: 150,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
+//              ),
+                    child: TextButton(
+                      child: const Text(
+                        'Show Details',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => setState(() {
+                        opacity = 1;
+                      }),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
+//              ),
+                    child: TextButton(
+                      child: const Text(
+                        'Show Details',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => setState(() {
+                        opacity = 1;
+                      }),
+                    ),
+                  ),
+                  Container(
+                    width: 150,
+                    height: 150,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
+//              ),
+                    child: TextButton(
+                      child: const Text(
+                        'Show Details',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => setState(() {
+                        opacity = 1;
+                      }),
+                    ),
+                  ),
+                  Container(
+                    width: 150,
+                    height: 150,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
+//              ),
+                    child: TextButton(
+                      child: const Text(
+                        'Show Details',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => setState(() {
+                        opacity = 1;
+                      }),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
+//              ),
+                    child: TextButton(
+                      child: const Text(
+                        'Show Details',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => setState(() {
+                        opacity = 1;
+                      }),
+                    ),
+                  ),
+                  Container(
+                    width: 150,
+                    height: 150,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
+//              ),
+                    child: TextButton(
+                      child: const Text(
+                        'Show Details',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => setState(() {
+                        opacity = 1;
+                      }),
+                    ),
+                  ),
+                  Container(
+                    width: 150,
+                    height: 150,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
+//              ),
+                    child: TextButton(
+                      child: const Text(
+                        'Show Details',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => setState(() {
+                        opacity = 1;
+                      }),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
+//              ),
+                    child: TextButton(
+                      child: const Text(
+                        'Show Details',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => setState(() {
+                        opacity = 1;
+                      }),
+                    ),
+                  ),
+                  Container(
+                    width: 150,
+                    height: 150,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
+//              ),
+                    child: TextButton(
+                      child: const Text(
+                        'Show Details',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => setState(() {
+                        opacity = 1;
+                      }),
+                    ),
+                  ),
+                  Container(
+                    width: 150,
+                    height: 150,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
+//              ),
+                    child: TextButton(
+                      child: const Text(
+                        'Show Details',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => setState(() {
+                        opacity = 1;
+                      }),
+                    ),
+                  ),
+                ],
+              ),
+            ]),
+          ),
         )
     );
   }
