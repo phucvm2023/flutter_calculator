bdc79ea79b519f5b76b8c0450f21685f3bec8e23
phucvm2023 <130111130+phucvm2023@users.noreply.github.com>
2023-04-11 08:23:47 +0700

    build 2023.04.11
    work well

--- flutter_git001_calculator/lib/main.dart
+++ flutter_git001_calculator/lib/main.dart
@@ -31,7 +31,56 @@ class scr006Route extends StatefulWidget {
 }
 
 class _scr006RouteState extends State<scr006Route> {
-  double opacity = 0.0;
+  String _output = "0";
+  String _input = "";
+  double _num1 = 0.0;
+  double _num2 = 0.0;
+  String _operation = "";
+
+  void _buttonPressed(String buttonText) {
+    setState(() {
+      if (buttonText == "C") {
+        _output = "0";
+        _input = "";
+        _num1 = 0.0;
+        _num2 = 0.0;
+        _operation = "";
+      } else if (buttonText == "+" || buttonText == "-" || buttonText == "x" || buttonText == "/") {
+        _num1 = double.parse(_input);
+        _operation = buttonText;
+        _output = "0";
+        _input = "";
+      } else if (buttonText == ".") {
+        if (_input.contains(".")) {
+          return;
+        } else {
+          _input = _input + buttonText;
+          _output = _input;
+        }
+      } else if (buttonText == "=") {
+        _num2 = double.parse(_input);
+        if (_operation == "+") {
+          _output = (_num1 + _num2).toStringAsFixed(2);
+        }
+        if (_operation == "-") {
+          _output = (_num1 - _num2).toStringAsFixed(2);
+        }
+        if (_operation == "x") {
+          _output = (_num1 * _num2).toStringAsFixed(2);
+        }
+        if (_operation == "/") {
+          _output = (_num1 / _num2).toStringAsFixed(2);
+        }
+        _num1 = 0.0;
+        _num2 = 0.0;
+        _input = "";
+        _operation = "";
+      } else {
+        _input = _input + buttonText;
+        _output = _input;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +94,7 @@ class _scr006RouteState extends State<scr006Route> {
               Row(
                 children: [
                   Text(
-                    'Hey!',
+                    _output,
                     style: TextStyle(
                       fontSize: 50,
                       fontFamily: 'Futura',
@@ -59,56 +108,50 @@ class _scr006RouteState extends State<scr006Route> {
                 children: [
                   Container(
                     margin: const EdgeInsets.all(4),
-                    width: 150,
-                    height: 150,
+                    width: 50,
+                    height: 50,
                     decoration: BoxDecoration(
         //                color: Colors.blue,
                         border: Border.all(),
                     ),
                     child: TextButton(
                       child: const Text(
-                        'Show Details',
+                        '1',
                         style: TextStyle(color: Colors.blueAccent),
                       ),
-                      onPressed: () => setState(() {
-                        opacity = 1;
-                      }),
+                      onPressed: () => _buttonPressed('1'),
                     ),
                   ),
                   Container(
                     margin: const EdgeInsets.all(4),
-                    width: 150,
-                    height: 150,
+                    width: 50,
+                    height: 50,
 //              decoration: BoxDecoration(
 //                color: Colors.blue,
 //                border: Border.all(),
 //              ),
                     child: TextButton(
                       child: const Text(
-                        'Show Details',
+                        '2',
                         style: TextStyle(color: Colors.blueAccent),
                       ),
-                      onPressed: () => setState(() {
-                        opacity = 1;
-                      }),
+                      onPressed: () => _buttonPressed('2'),
                     ),
                   ),
                   Container(
                     margin: const EdgeInsets.all(4),
-                    width: 150,
-                    height: 150,
+                    width: 50,
+                    height: 50,
 //              decoration: BoxDecoration(
 //                color: Colors.blue,
 //                border: Border.all(),
 //              ),
                     child: TextButton(
                       child: const Text(
-                        'Show Details',
+                        '3',
                         style: TextStyle(color: Colors.blueAccent),
                       ),
-                      onPressed: () => setState(() {
-                        opacity = 1;
-                      }),
+                      onPressed: () => _buttonPressed('3'),
                     ),
                   ),
                 ],
@@ -116,109 +159,52 @@ class _scr006RouteState extends State<scr006Route> {
               Row(
                 children: [
                   Container(
-                    width: 150,
-                    height: 150,
+                    margin: const EdgeInsets.all(4),
+                    width: 50,
+                    height: 50,
 //              decoration: BoxDecoration(
 //                color: Colors.blue,
 //                border: Border.all(),
 //              ),
                     child: TextButton(
                       child: const Text(
-                        'Show Details',
+                        '4',
                         style: TextStyle(color: Colors.blueAccent),
                       ),
-                      onPressed: () => setState(() {
-                        opacity = 1;
-                      }),
+                      onPressed: () => _buttonPressed('4'),
                     ),
                   ),
                   Container(
-                    width: 150,
-                    height: 150,
+                    margin: const EdgeInsets.all(4),
+                    width: 50,
+                    height: 50,
 //              decoration: BoxDecoration(
 //                color: Colors.blue,
 //                border: Border.all(),
 //              ),
                     child: TextButton(
                       child: const Text(
-                        'Show Details',
+                        '5',
                         style: TextStyle(color: Colors.blueAccent),
                       ),
-                      onPressed: () => setState(() {
-                        opacity = 1;
-                      }),
+                      onPressed: () => _buttonPressed('5'),
+
                     ),
                   ),
                   Container(
-                    width: 150,
-                    height: 150,
+                    margin: const EdgeInsets.all(4),
+                    width: 50,
+                    height: 50,
 //              decoration: BoxDecoration(
 //                color: Colors.blue,
 //                border: Border.all(),
 //              ),
                     child: TextButton(
                       child: const Text(
-                        'Show Details',
+                        '6',
                         style: TextStyle(color: Colors.blueAccent),
                       ),
-                      onPressed: () => setState(() {
-                        opacity = 1;
-                      }),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Container(
-                    width: 150,
-                    height: 150,
-//              decoration: BoxDecoration(
-//                color: Colors.blue,
-//                border: Border.all(),
-//              ),
-                    child: TextButton(
-                      child: const Text(
-                        'Show Details',
-                        style: TextStyle(color: Colors.blueAccent),
-                      ),
-                      onPressed: () => setState(() {
-                        opacity = 1;
-                      }),
-                    ),
-                  ),
-                  Container(
-                    width: 150,
-                    height: 150,
-//              decoration: BoxDecoration(
-//                color: Colors.blue,
-//                border: Border.all(),
-//              ),
-                    child: TextButton(
-                      child: const Text(
-                        'Show Details',
-                        style: TextStyle(color: Colors.blueAccent),
-                      ),
-                      onPressed: () => setState(() {
-                        opacity = 1;
-                      }),
-                    ),
-                  ),
-                  Container(
-                    width: 150,
-                    height: 150,
-//              decoration: BoxDecoration(
-//                color: Colors.blue,
-//                border: Border.all(),
-//              ),
-                    child: TextButton(
-                      child: const Text(
-                        'Show Details',
-                        style: TextStyle(color: Colors.blueAccent),
-                      ),
-                      onPressed: () => setState(() {
-                        opacity = 1;
-                      }),
+                      onPressed: () => _buttonPressed('6'),
                     ),
                   ),
                 ],
@@ -226,54 +212,103 @@ class _scr006RouteState extends State<scr006Route> {
               Row(
                 children: [
                   Container(
-                    width: 150,
-                    height: 150,
+                    margin: const EdgeInsets.all(4),
+                    width: 50,
+                    height: 50,
 //              decoration: BoxDecoration(
 //                color: Colors.blue,
 //                border: Border.all(),
 //              ),
                     child: TextButton(
                       child: const Text(
-                        'Show Details',
+                        '7',
                         style: TextStyle(color: Colors.blueAccent),
                       ),
-                      onPressed: () => setState(() {
-                        opacity = 1;
-                      }),
+                      onPressed: () => _buttonPressed('7'),
                     ),
                   ),
                   Container(
-                    width: 150,
-                    height: 150,
+                    margin: const EdgeInsets.all(4),
+                    width: 50,
+                    height: 50,
 //              decoration: BoxDecoration(
 //                color: Colors.blue,
 //                border: Border.all(),
 //              ),
                     child: TextButton(
                       child: const Text(
-                        'Show Details',
+                        '8',
                         style: TextStyle(color: Colors.blueAccent),
                       ),
-                      onPressed: () => setState(() {
-                        opacity = 1;
-                      }),
+                      onPressed: () => _buttonPressed('8'),
                     ),
                   ),
                   Container(
-                    width: 150,
-                    height: 150,
+                    margin: const EdgeInsets.all(4),
+                    width: 50,
+                    height: 50,
 //              decoration: BoxDecoration(
 //                color: Colors.blue,
 //                border: Border.all(),
 //              ),
                     child: TextButton(
                       child: const Text(
-                        'Show Details',
+                        '9',
                         style: TextStyle(color: Colors.blueAccent),
                       ),
-                      onPressed: () => setState(() {
-                        opacity = 1;
-                      }),
+                      onPressed: () => _buttonPressed('9'),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(4),
+                    width: 50,
+                    height: 50,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
+//              ),
+                    child: TextButton(
+                      child: const Text(
+                        '+',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => _buttonPressed('+'),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(4),
+                    width: 50,
+                    height: 50,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
+//              ),
+                    child: TextButton(
+                      child: const Text(
+                        '-',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => _buttonPressed('-'),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(4),
+                    width: 50,
+                    height: 50,
+//              decoration: BoxDecoration(
+//                color: Colors.blue,
+//                border: Border.all(),
+//              ),
+                    child: TextButton(
+                      child: const Text(
+                        '=',
+                        style: TextStyle(color: Colors.blueAccent),
+                      ),
+                      onPressed: () => _buttonPressed('='),
                     ),
                   ),
                 ],
